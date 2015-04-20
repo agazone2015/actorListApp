9ce1a1c511d777d7d286554c1a2e2c0aba4e70da
Chappie <chappie@Chappie.local>
2015-04-21 09:04:38 +1000

    Initial Commit

---
+++ actorListApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  actorListApp
+//
+//  Created by Chappie on 21/04/2015.
+//  Copyright (c) 2015 Chappie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ actorListAppTests/actorListAppTests.m
@@ -0,0 +1,40 @@
+//
+//  actorListAppTests.m
+//  actorListAppTests
+//
+//  Created by Chappie on 21/04/2015.
+//  Copyright (c) 2015 Chappie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface actorListAppTests : XCTestCase
+
+@end
+
+@implementation actorListAppTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
